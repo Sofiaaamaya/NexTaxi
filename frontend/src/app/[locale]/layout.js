e2ad1b8a8c6aa6c949679647a51c41245805aafc
Sofiaaamaya <soofiimaya@gmail.com>
2026-05-20 03:23:57 +0100

    "Fix: redireccion"

--- frontend/src/app/[locale]/layout.js
+++ frontend/src/app/[locale]/layout.js
@@ -1,5 +1,4 @@
 import { NextIntlClientProvider } from 'next-intl';
-import { notFound } from 'next/navigation';
 import '@/styles/global.css';
 import { AuthProvider } from '@/context/AuthContext';
 import LayoutWrapper from '@/components/common/LayoutWrapper';
@@ -14,7 +13,6 @@ export const metadata = {
   description:
     'NexTaxi ofrece traslados rápidos, seguros y cómodos. Reserva tu viaje en línea y disfruta de la mejor experiencia en transporte.',
 };
-
 
 export default async function LocaleLayout({ children, params }) {
   const { locale } = params;
@@ -33,8 +31,8 @@ export default async function LocaleLayout({ children, params }) {
   }
 
   return (
-    <html lang={locale} className={poppins.variable}>
-      <body className={poppins.className}>
+    <html lang={locale}>
+      <body>
         <NextIntlClientProvider locale={locale} messages={messages}>
           <AuthProvider>
             <LayoutWrapper>{children}</LayoutWrapper>
