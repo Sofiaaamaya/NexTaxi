9596902e2d6fa6c11e9fb83e92d2b32e6b613b37
Sofiaaamaya <soofiimaya@gmail.com>
2026-05-20 03:37:57 +0100

    Revert ""fix: layout""
    This reverts commit dd0779a78236ca93ed47e32f2191326f6a9aef49.

--- frontend/src/app/[locale]/layout.js
+++ frontend/src/app/[locale]/layout.js
@@ -3,6 +3,14 @@ import { notFound } from 'next/navigation';
 import '@/styles/global.css';
 import { AuthProvider } from '@/context/AuthContext';
 import LayoutWrapper from '@/components/common/LayoutWrapper';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700', '800'],
+  variable: '--font-poppins',
+  display: 'swap',
+});
 
 export const viewport = {
   width: 'device-width',
@@ -15,26 +23,29 @@ export const metadata = {
     'NexTaxi ofrece traslados rápidos, seguros y cómodos. Reserva tu viaje en línea y disfruta de la mejor experiencia en transporte.',
 };
 
-
 export default async function LocaleLayout({ children, params }) {
-  const { locale } = params;
+  const { locale } = await params;
 
-  let messages = {};
+  // CARGA COMPATIBLE CON EDGE RUNTIME
+  const res = await fetch(`/locales/${locale}.json`, { cache: 'no-store' });
 
-  try {
-    const res = await fetch(`/locales/${locale}.json`, { cache: 'no-store' });
-    if (res.ok) {
-      messages = await res.json();
-    } else {
-      console.error(`No se pudo cargar /locales/${locale}.json`);
-    }
-  } catch (err) {
-    console.error("Error cargando traducciones:", err);
-  }
+
+
+  const messages = await res.json();
 
   return (
-    <html lang={locale} className={poppins.variable}>
-      <body className={poppins.className}>
+    <html lang={locale} suppressHydrationWarning className={poppins.variable}>
+      <head>
+        <script
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
+          async
+        ></script>
+      </head>
+
+      <body
+        className={`${poppins.className} bg-background text-textPrimary antialiased`}
+        suppressHydrationWarning
+      >
         <NextIntlClientProvider locale={locale} messages={messages}>
           <AuthProvider>
             <LayoutWrapper>{children}</LayoutWrapper>
