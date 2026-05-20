dd0779a78236ca93ed47e32f2191326f6a9aef49
Sofiaaamaya <soofiimaya@gmail.com>
2026-05-20 03:18:45 +0100

    "fix: layout"

--- frontend/src/app/[locale]/layout.js
+++ frontend/src/app/[locale]/layout.js
@@ -3,14 +3,6 @@ import { notFound } from 'next/navigation';
 import '@/styles/global.css';
 import { AuthProvider } from '@/context/AuthContext';
 import LayoutWrapper from '@/components/common/LayoutWrapper';
-import { Poppins } from 'next/font/google';
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700', '800'],
-  variable: '--font-poppins',
-  display: 'swap',
-});
 
 export const viewport = {
   width: 'device-width',
@@ -23,29 +15,26 @@ export const metadata = {
     'NexTaxi ofrece traslados rápidos, seguros y cómodos. Reserva tu viaje en línea y disfruta de la mejor experiencia en transporte.',
 };
 
+
 export default async function LocaleLayout({ children, params }) {
-  const { locale } = await params;
+  const { locale } = params;
 
-  // CARGA COMPATIBLE CON EDGE RUNTIME
-  const res = await fetch(`/locales/${locale}.json`, { cache: 'no-store' });
+  let messages = {};
 
-
-
-  const messages = await res.json();
+  try {
+    const res = await fetch(`/locales/${locale}.json`, { cache: 'no-store' });
+    if (res.ok) {
+      messages = await res.json();
+    } else {
+      console.error(`No se pudo cargar /locales/${locale}.json`);
+    }
+  } catch (err) {
+    console.error("Error cargando traducciones:", err);
+  }
 
   return (
-    <html lang={locale} suppressHydrationWarning className={poppins.variable}>
-      <head>
-        <script
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
-          async
-        ></script>
-      </head>
-
-      <body
-        className={`${poppins.className} bg-background text-textPrimary antialiased`}
-        suppressHydrationWarning
-      >
+    <html lang={locale} className={poppins.variable}>
+      <body className={poppins.className}>
         <NextIntlClientProvider locale={locale} messages={messages}>
           <AuthProvider>
             <LayoutWrapper>{children}</LayoutWrapper>
